038c263e1d5c673d360aa6bb0f81572224cd3e91
Ernesto García Bernal <ern.garbe@gmail.com>
2022-11-09 21:02:13 -0600

    first commit

---
+++ js/database.js
@@ -0,0 +1,16 @@
+const sliderDb = [
+  {
+    title:"Encuentra la mejor ropa",
+    src:"./img/image1.png"
+  },
+  {
+    title:"Regala algo especial",
+    src:"./img/image3.png"
+  },
+  {
+    title:"Pasa horas de diversión",
+    src:"./img/image2.png"
+  },
+
+]
+
